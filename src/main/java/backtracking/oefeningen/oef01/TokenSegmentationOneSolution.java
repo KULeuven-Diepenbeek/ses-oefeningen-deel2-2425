041e3712ff8d242b6a437a21eb5f4aa91e7e3a00
Koen Yskout <koen.yskout@kuleuven.be>
2025-04-30 12:05:51 +0200

    backtracking - oef 1 uit de les

--- src/main/java/backtracking/oefeningen/oef01/TokenSegmentationOneSolution.java
+++ src/main/java/backtracking/oefeningen/oef01/TokenSegmentationOneSolution.java
@@ -1,4 +1,4 @@
-package backtracking.demo;
+package backtracking.oefeningen.oef01;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,27 +14,31 @@ public class TokenSegmentationOneSolution {
             String strToTokenize,
             List<String> tokensSoFar
     ) {
-        System.out.println("'" + strToTokenize + "' " + tokensSoFar);
-       if (strToTokenize.isEmpty()) return tokensSoFar;
+        if (strToTokenize.isEmpty()) return tokensSoFar;
+        if (tokens.isEmpty()) return null;
 
-       for (var token : tokens) {
-           if (strToTokenize.startsWith(token)) {
-               String rest = strToTokenize.substring(token.length());
-               tokensSoFar.add(token);
-               var solution = findSegmentation_worker(tokens, rest, tokensSoFar);
-               if (solution != null) {
-                   return solution;
-               } else {
-                   // no solution
-                   tokensSoFar.removeLast();
-               }
-           }
-       }
-       return null;
+        for (var token : tokens) {
+            if (strToTokenize.startsWith(token)) {
+                String rest = strToTokenize.substring(token.length());
+                tokensSoFar.add(token);
+                // tokens.remove(token)
+                var newTokens = new ArrayList<>(tokens);
+                newTokens.remove(token);
+                var solution = findSegmentation_worker(newTokens, rest, tokensSoFar);
+                if (solution != null) {
+                    return solution;
+                } else {
+                    // no solution
+                    // tokens.add(token)
+                    tokensSoFar.removeLast();
+                }
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        var tokens = List.of("s", "an", "ca", "cat", "dog", "and", "sand", "dogs");
+        var tokens = new ArrayList<>(List.of("s", "an", "ca", "cat", "dog", "and", "sand", "dogs"));
         var str = "catsanddogs";
         var solution = findSegmentation(tokens, str);
         System.out.println(solution);
